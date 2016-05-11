0b9877488fb9cefabe1678372cf016ef251a61ef
AlegriaForman <alegriaforman@gmail.com>
2016-05-11 12:58:56 -0700

    add eslintify and protractor assignment

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# basic_angular_app
+# basic_angular_app with eslintify and protractor.
+
+1. Type node server.js to start server 5000
+2. Type webdriver-manager start to start Selenium
+3. Type protractor test_and_integration/config.js
+4. Type gulp lint:client - to test the eslint from the client side
+5. Type gulp lint:server - to test the eslint from the server side
+6. Type gulp protractor:test - to test for protractor test
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const eslint = require('gulp-eslint');
+
+var paths = ['**/*.js', '!build/**', '!node_modules/**', '!test_and_integration/**'];
 
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
     .pipe(webpack({
+      devtool: 'source-map',
       output: {
         filename: 'bundle.js'
       }
@@ -16,5 +20,28 @@ gulp.task('static:dev', () => {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev']);
+gulp.task('protractor:test', () => {
+  gulp.src('test_and_integration/config.js');
+});
+
+gulp.task('lint:nontest', () => {
+  return gulp.src(paths)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:client', () => {
+  gulp.src('app/**/**.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:server', () => {
+  gulp.src('**.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'protractor:test']);
 gulp.task('default', ['build:dev']);
+gulp.task('lint', ['lint:nontest', 'lint:client', 'lint:server']);
